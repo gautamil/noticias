3c948aa50d3ecb576e63b974f1a70789390846c2
arieelbernal <arieelbernal@gmail.com>
2021-09-29 15:38:16 -0300

    solucion a errores

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
 window.addEventListener('load',function(){
     const contNoticia= document.querySelector(".bloquesNoticias");
-    renderizadoNoticias();
-})
-
-
-function renderizadoNoticias(){
-    for(let i=0; i<listado.length; i++){
-        contNoticia.innerHTML += `<article>
-        <img src="${listadoNoticias.imgUrl}" alt="Imagen noticia">
-        <h2>
-            ${listadoNoticias.titulo}
-        </h2>
-        <p>
-            ${listadoNoticias.descripcion}
-        </p>
-        <p>
-            ${listadoNoticias.fecha}
-        </p>
-        </article>`
+    console.log(contNoticia);
+    renderizadoNoticias(noticias);
+    function renderizadoNoticias(listado){
+        for(let i=0; i<listado.length; i++){
+            contNoticia.innerHTML += `<article>
+            <img src="${listado[i].imgUrl}" alt="Imagen noticia">
+            <h2>
+                ${listado[i].titulo}
+            </h2>
+            <p>
+                ${listado[i].descripcion}
+            </p>
+            <p>
+                ${listado[i].fecha}
+            </p>
+            </article>`;
+            console.log(contNoticia);
+        }
     }
-}
+})
